17f58dbc2d628fcd4a81232486720ad3c9619bd8
okatsuo <rafael.goncanna@gmail.com>
2022-08-20 17:51:48 -0300

    move imports to correct order

--- federationGateway/src/index.ts
+++ federationGateway/src/index.ts
@@ -1,6 +1,6 @@
-import { ApolloGateway } from '@apollo/gateway'
-import { ApolloServer } from 'apollo-server'
 import 'reflect-metadata'
+import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway'
+import { ApolloServer } from 'apollo-server'
 
 type AppOption = {
   port: number
@@ -9,9 +9,11 @@ type AppOption = {
 class App {
   static async start ({ port }: AppOption) {
     const gateway = new ApolloGateway({
-      serviceList: [{
-        name: 'User', url: 'http://localhost:4001/'
-      }]
+      supergraphSdl: new IntrospectAndCompose({
+        subgraphs: [
+          { name: 'user', url: 'http://localhost:4001/graphql' }
+        ]
+      })
     })
 
     const { executor, schema } = await gateway.load()
